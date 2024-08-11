0e176bbc4918c31fad39f806338b89c407da9008
Pranshu Verma <pranshuverma601@gmail.com>
2024-08-11 13:39:26 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/my-portfolio/',
@@ -9,4 +8,4 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
   },
-})
+});
